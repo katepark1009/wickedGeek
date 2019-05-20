57d46b696c388347cf549c0ee5f6c36ca3e5f165
kate park <rudhi1009@gmail.com>
2019-05-20 13:07:18 -0700

    Added componentDidMount and render

---
+++ client/components/product-details.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product : null
+    };
+  }
+  componentDidMount() {
+    const id = ;
+    fetch(`/api/products?id=${id}`, {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(json => this.setState({ products: json }));
+  }
+  render() {
+    return(
+      <div className = "product__detail__container">
+        <div className="back">Back to catalog</div>
+        <div className="product__detail__card">
+          <div className="product__detail__image">
+            <img src="" alt=""/>
+            <div className="product__detail__info">
+                <div className="product__detail__title"></div>
+                <div className="product__detail__price"></div>
+                <div className="product__detail__description"></div>
+            </div>
+          </div>
+        </div>
+        <div className="product__detail__all__info"></div>
+      </div>
+    );
+  }
+}
