b82a7008ddbd78990b46992adab22b09aa5a5698
kate park <rudhi1009@gmail.com>
2019-05-20 13:08:27 -0700

    Added state to App

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,6 +6,8 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: 'catalog',
+      params: {},
       products: []
     };
   }
