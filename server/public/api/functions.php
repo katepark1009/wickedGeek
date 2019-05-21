655b1b6edb86e31fbd6fea36e6dc6df2fd4a4339
kate park <rudhi1009@gmail.com>
2019-05-21 15:49:00 -0700

    Completed create-products-back-end

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,14 +1,8 @@
 <?php 
    function error_handler($error) {
-    return null;
-  };
+     $output =array("success" => false, "error" => $error-> getMessage() );
+     $json_output = json_encode($output);
+     print ($json_output);
+   };
    set_exception_handler("error_handler");
-// //   doStuff();
-// //   throw new Exception('I want an error');
-//    $randomNumber = rand(1, 10);
-//   if ($randomNumber % 2) {
-//       print "The number was {$randomNumber}";
-//   } else {
-//       throw new Exception('Exception: the number was even');
-//   };
 ?> 
