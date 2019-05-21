abdeb421661ce2aceae1602ba6e417c1aa90781f
kate park <rudhi1009@gmail.com>
2019-05-21 16:12:01 -0700

    completed create-products

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,6 +1,5 @@
 <?php
-require_once('./functions.php');
-dostuff();
+require_once('functions.php');
 set_exception_handler("error_handler");
 $output = file_get_contents('./dummy-products-list.json');
 print $output;
