74c2de5c82596108c9fcfc7bf81fef864d20657f
kate park <rudhi1009@gmail.com>
2019-05-21 16:02:56 -0700

    server error 500

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -2,6 +2,7 @@
    function error_handler($error) {
      $output =array("success" => false, "error" => $error-> getMessage() );
      $json_output = json_encode($output);
+     http_response_code(500);
      print ($json_output);
    };
    set_exception_handler("error_handler");
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,5 +1,6 @@
 <?php
 require_once('./functions.php');
+dostuff();
 set_exception_handler("error_handler");
 $output = file_get_contents('./dummy-products-list.json');
 print $output;
