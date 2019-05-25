858b74ab3564f476435118bbec9fd6faf7eeb5fd
kate park <rudhi1009@gmail.com>
2019-05-25 01:58:34 -0700

    changed but no url error

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -5,24 +5,29 @@
   if(!$conn) {
       throw new Exception("Connection error: " . mysqli_connect_error());
   };
-  $id = $_GET['id'];
+  $query='';
   if(empty($_GET['id'])) {
-    $whereClause = '';
+    // $whereClause = '';
+    $query = "SELECT p.* , i.`url` FROM `products_list` AS p JOIN `images` as i ON p.`id` = i.`products_id` ORDER BY p.`id`";
   } else {
+    $id = $_GET['id'];
     if(is_numeric($id)){
-      $whereClause = "WHERE id={$id}";
+      // $whereClause = "WHERE id={$id}";
+      $query = "SELECT * FROM products_list WHERE id={$id}";
     } else {
       throw new Exception("Errormessage: 'id' needs to be a number");
     };
   };
-  $query = "SELECT * FROM products_list " . $whereClause;
+  // $query = "SELECT * FROM products_list " . $whereClause;
   $result = mysqli_query($conn, $query);
   
   if (!$result) {
     throw new Exception("Errormessage: " . mysqli_error($conn));
   };
+
   if(!empty($_GET['id'])) {
     if(mysqli_num_rows($result) < 1 ){
+      $id = $_GET['id'];
       throw new Exception("Errormessage: invalid ID: {$id}");
     };
   };
@@ -30,7 +35,16 @@
   $output = [];
 
   while ($row = mysqli_fetch_assoc($result)) {
-    $output[] = $row;
+    $thisData = array($row['image']);
+    unset($row['id'], $row['name'], $row['price'], $row['shortDescription']);
+
+    if(empty($output[$row['id']])) {
+      $row['image'] = [];
+      $row['image'][] = $thisData;
+      $output[$row['id']] = $row; 
+    } else {
+      $output[$row['id']]['image'][] = $thisData;
+    }
   };
 
   $json_output = json_encode($output, JSON_INVALID_UTF8_SUBSTITUTE);
